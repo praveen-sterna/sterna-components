67d364409b46616bc4fdf5c69e7397aec0e005ae
Praveen B <sterna@Praveens-MacBook-Air.local>
2023-05-30 13:20:37 +0530

    initial commit

--- lib/utills/sterna_constants.dart
+++ lib/utills/sterna_constants.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 class SternaConstants{
   static Color primary = const Color(0xFF0f3cc9);
 
-  void setPrimaryColor(Color color){
+  static void setPrimaryColor(Color color){
     primary = color;
   }
 }
