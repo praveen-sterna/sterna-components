eaf1ce3ac843a6303a5c7607e8c4af0929b798f3
Praveen B <sterna@Praveens-MacBook-Air.local>
2023-06-07 16:05:43 +0530

    fixed bugs

--- lib/components/custom_appbar.dart
+++ lib/components/custom_appbar.dart
@@ -8,7 +8,8 @@ class CustomAppBar extends StatelessWidget implements PreferredSize{
   final IconData? leadingIcon;
   final Color? bgColor;
   final List<Widget>? actions;
-  const CustomAppBar({Key? key, required this.title, this.centerTitle, this.isLeading, this.leadingIcon, this.bgColor, this.actions}) : super(key: key);
+  final Color? titleColor;
+  const CustomAppBar({Key? key, required this.title, this.centerTitle, this.isLeading, this.leadingIcon, this.bgColor, this.actions, this.titleColor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +21,10 @@ class CustomAppBar extends StatelessWidget implements PreferredSize{
         onPressed: (){
           Navigation.instance.goBack();
         },
-        icon: Icon(leadingIcon ?? Icons.keyboard_backspace_rounded, color: Colors.white,),
+        icon: Icon(leadingIcon ?? Icons.keyboard_backspace_rounded, color: titleColor ?? Colors.white,),
       ) : null,
       actions: actions,
-      title: Text(title,style: const TextStyle(color: Colors.white, fontSize: 17, fontWeight: FontWeight.w600),),
+      title: Text(title,style: TextStyle(color: titleColor ?? Colors.white, fontSize: 17, fontWeight: FontWeight.w600),),
       centerTitle: centerTitle ?? false,
     );
   }
