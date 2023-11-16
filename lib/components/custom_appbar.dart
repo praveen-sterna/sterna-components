4c1de592a90e40ae64bc13a7b9eabf63b90075c5
Praveen B <sterna@Praveens-MacBook-Air.local>
2023-11-16 13:16:18 +0530

    fixed bugs

--- lib/components/custom_appbar.dart
+++ lib/components/custom_appbar.dart
@@ -8,22 +8,13 @@ class CustomAppBar extends StatelessWidget implements PreferredSize{
   final bool? isLeading;
   final IconData? leadingIcon;
   final List<Widget>? actions;
-  final double? toolbarHeight;
-  const CustomAppBar({Key? key, required this.title, this.centerTitle, this.isLeading, this.leadingIcon, this.actions, this.toolbarHeight}) : super(key: key);
+  const CustomAppBar({Key? key, required this.title, this.centerTitle, this.isLeading, this.leadingIcon, this.actions}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      toolbarHeight: toolbarHeight ?? 50,
       backgroundColor: SternaConstants.appBarColor,
       elevation: 0,
-      systemOverlayStyle: SystemUiOverlayStyle(
-          statusBarBrightness: Brightness.light,
-          statusBarColor: SternaConstants.appBarColor,
-          systemNavigationBarColor: Colors.white,
-          systemNavigationBarIconBrightness: Brightness.dark,
-          systemNavigationBarDividerColor: Colors.white
-      ),
       automaticallyImplyLeading: false,
       leading: (isLeading ?? true) ? IconButton(
         onPressed: (){
